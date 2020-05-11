c67f9cd33a8896b92d03b09d37be19222cbcf650
siddheshlaud <sidd.laud@gmail.com>
2020-05-11 12:19:16 +0530

    HTTP Headers and Query Params

--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
 
 import { Post } from "./post.model";
 import { map, catchError } from "rxjs/operators";
@@ -7,8 +7,7 @@ import { Subject, throwError } from "rxjs";
 
 @Injectable()
 export class PostsService {
-
-  error:Subject<string> = new Subject<string>();
+  error: Subject<string> = new Subject<string>();
   constructor(private http: HttpClient) {}
 
   createAndStrorePost(title: string, content: string) {
@@ -21,17 +20,28 @@ export class PostsService {
       )
       .subscribe(
         responseData => {
-        console.log(responseData);
+          console.log(responseData);
         },
         error => {
           this.error.next(error.message);
-        });
+        }
+      );
   }
 
   fetchPosts() {
+    let searchParams = new HttpParams();
+    searchParams = searchParams.append("print", "pretty"); //HttpParams object is immutable so needs to be assigned again
+    searchParams = searchParams.append("custom", "key");
     return this.http
       .get<{ [key: string]: Post }>(
-        "https://angular-recipe-61972.firebaseio.com/posts.json"
+        "https://angular-recipe-61972.firebaseio.com/posts.json",
+        {
+          headers: new HttpHeaders({
+            "Custom-Header1": "Hello",
+            "Custom-Header2": "World"
+          }),
+          params: searchParams
+        }
       )
       .pipe(
         map(responseData => {
@@ -43,22 +53,19 @@ export class PostsService {
           }
           return postArray;
         }),
-        catchError(
-          errorRes => {
-            //Log error
-            //Send to analytics
-            //Generic work on errors
+        catchError(errorRes => {
+          //Log error
+          //Send to analytics
+          //Generic work on errors
 
-            //Its important to throw the error back
-            return throwError(errorRes);
-          }
-        )
+          //Its important to throw the error back
+          return throwError(errorRes);
+        })
       );
   }
 
   deletePosts() {
-    return this.http
-    .delete(
+    return this.http.delete(
       "https://angular-recipe-61972.firebaseio.com/posts.json"
     );
   }
