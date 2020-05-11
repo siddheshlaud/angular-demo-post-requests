8ae590cffedac436abbe4ad1fa9960031f04db76
siddheshlaud <sidd.laud@gmail.com>
2020-05-11 12:41:03 +0530

    tap on Http Events and Modify Response TYpe

--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from "@angular/core";
-import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
+import { HttpClient, HttpHeaders, HttpParams, HttpEventType } from "@angular/common/http";
 
 import { Post } from "./post.model";
-import { map, catchError } from "rxjs/operators";
+import { map, catchError, tap } from "rxjs/operators";
 import { Subject, throwError } from "rxjs";
 
 @Injectable()
@@ -16,11 +16,15 @@ export class PostsService {
     this.http
       .post<{ name: string }>(
         "https://angular-recipe-61972.firebaseio.com/posts.json",
-        postData
+        postData,
+        {
+          observe: 'response' //Be deafult we get extracted body. This is used to get full response object
+        }
       )
       .subscribe(
         responseData => {
-          console.log(responseData);
+          console.log(responseData); // Full Response object with actual headers
+          console.log(responseData.body); //Actual Body content
         },
         error => {
           this.error.next(error.message);
@@ -66,7 +70,21 @@ export class PostsService {
 
   deletePosts() {
     return this.http.delete(
-      "https://angular-recipe-61972.firebaseio.com/posts.json"
-    );
+      "https://angular-recipe-61972.firebaseio.com/posts.json",
+      {
+        observe: 'events',
+        responseType: 'text' //By default json, we can change this using this
+      }
+    ).pipe(tap(
+      event => {
+        console.log(event);
+        if(event.type === HttpEventType.Sent) {
+          console.log('Http Request Sent');
+        }
+        if(event.type === HttpEventType.Response) {
+          console.log(event.body);
+        }
+      }
+    ));
   }
 }
